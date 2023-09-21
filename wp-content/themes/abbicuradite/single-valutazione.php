ee27731468d59b5d52a6ef3d0944387f4a807a1d
kella <kella@a-piu.it>
2023-09-21 12:34:20 +0200

    🐛 VALUTAZIONE > Not acces page  only when user login

--- wp-content/themes/abbicuradite/single-valutazione.php
+++ wp-content/themes/abbicuradite/single-valutazione.php
@@ -1,31 +1,35 @@
 <?php
 get_header();
-require_once __DIR__ .'/includes/BlockValutazione.php';
-require_once __DIR__ .'/includes/BlockAnagrafici.php';
-$domandeAnagrafici = get_field("domande_anagrafici");
-$domande = get_field("valutazione");
-$userId= get_current_user_id();
+require_once __DIR__.'/includes/BlockValutazione.php';
+require_once __DIR__.'/includes/BlockAnagrafici.php';
+$domandeAnagrafici = get_field('domande_anagrafici');
+$domande = get_field('valutazione');
+$userId = get_current_user_id();
 // get all Request and answers
 $datas = [];
-foreach ($domande as $d => $domanda) :
+foreach ($domande as $d => $domanda) {
     $data['dommanda'] = $d;
-    foreach ($domanda["risposte"] as $key => $risposta) :
-        if($risposta["fascia_verde"])
-            $colorAnswer = "verde";
-        if($risposta["fascia_giallo"])
-            $colorAnswer = "giallo";
-        if($risposta["fascia_arancione"])
-            $colorAnswer = "arancione";
-        if($risposta["fascia_rossa"])
-            $colorAnswer = "rossa";
-        $arr = array(
-            "idQuestion" => $d,
-            "idAnswer" => $key,
-            "color" => $colorAnswer,
-        );
+    foreach ($domanda['risposte'] as $key => $risposta) {
+        if ($risposta['fascia_verde']) {
+            $colorAnswer = 'verde';
+        }
+        if ($risposta['fascia_giallo']) {
+            $colorAnswer = 'giallo';
+        }
+        if ($risposta['fascia_arancione']) {
+            $colorAnswer = 'arancione';
+        }
+        if ($risposta['fascia_rossa']) {
+            $colorAnswer = 'rossa';
+        }
+        $arr = [
+            'idQuestion' => $d,
+            'idAnswer' => $key,
+            'color' => $colorAnswer,
+        ];
         array_push($datas, $arr);
-    endforeach;
-endforeach;
+    }
+}
 
 $id_page_valutazione = 290;
 ?>
@@ -33,7 +37,7 @@ $id_page_valutazione = 290;
 
     <div class="container p-5 d-flex justify-content-center">
         <?php
-        while(have_posts()):
+        while (have_posts()) {
             the_post();
             ?>
             <div class="row my-5">
@@ -45,21 +49,23 @@ $id_page_valutazione = 290;
                 </div>
             </div>
 
-        <?php endwhile; ?>
+        <?php } ?>
     </div>
 </div>
 
+<?php
 
-<form action="" method="post" id="form-valutazione" class="formTestRischi" enctype="multipart/form-data" data-url="<?php echo admin_url("admin-ajax.php"); ?>">
+if (is_user_logged_in()) {?>
+<form action="" method="post" id="form-valutazione" class="formTestRischi" enctype="multipart/form-data" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
     <input type="hidden" name="action" value="add_test_valutazione_user">
-    <input type="hidden" name="userID" id="idUser" value="<?php echo $userId;?>">
-    <input type="hidden" name="idTest" id="idTest" value="<?php echo get_the_ID();?>">
+    <input type="hidden" name="userID" id="idUser" value="<?php echo $userId; ?>">
+    <input type="hidden" name="idTest" id="idTest" value="<?php echo get_the_ID(); ?>">
 
     <div class="main quiz">
         <div class="container">
             <div class="cnt-h5">
-                <h5><?php echo get_field("titolo_blocco_dati_anagrafici_questionario", $id_page_valutazione); ?></h5>
-                <span><?php echo get_field("sottotitolo_blocco_dati_anagrafici_questionario", $id_page_valutazione); ?></span>
+                <h5><?php echo get_field('titolo_blocco_dati_anagrafici_questionario', $id_page_valutazione); ?></h5>
+                <span><?php echo get_field('sottotitolo_blocco_dati_anagrafici_questionario', $id_page_valutazione); ?></span>
             </div>
 
         </div>
@@ -70,7 +76,7 @@ $id_page_valutazione = 290;
             <div class="valutazione mt-4">
                 <fieldset class="field ">
                     <div class="row gy-5 gx-5">
-                    <?php  blockAnagrafici($domandeAnagrafici,$userId); ?>
+                    <?php blockAnagrafici($domandeAnagrafici, $userId); ?>
                     </div>
                 </fieldset>
             </div>
@@ -81,8 +87,8 @@ $id_page_valutazione = 290;
     <div class="main quiz mt-5 pt-5">
         <div class="container">
             <div class="cnt-h5">
-                <h5><?php echo get_field("titolo_blocco_valutazione_questionario", $id_page_valutazione); ?></h5>
-                <span><?php echo get_field("sottotitolo_blocco_valutazione_questionario", $id_page_valutazione); ?></span>
+                <h5><?php echo get_field('titolo_blocco_valutazione_questionario', $id_page_valutazione); ?></h5>
+                <span><?php echo get_field('sottotitolo_blocco_valutazione_questionario', $id_page_valutazione); ?></span>
             </div>
         </div>
     </div>
@@ -92,7 +98,7 @@ $id_page_valutazione = 290;
             <div class="valutazione mt-4">
                 <fieldset class="field ">
                     <div class="row gy-5 gx-5">
-                        <?php  blockValutazione($domande,$userId); ?>
+                        <?php blockValutazione($domande, $userId); ?>
                     </div>
                 </fieldset>
             </div>
@@ -103,11 +109,14 @@ $id_page_valutazione = 290;
         <div class="container py-4">
             <div class="valutazione">
                 <div class="my-3 text-center">
-                    <button type="button" class="btn-scopri btn-send-valutazione ps-3 pe-3 pt-2 pb-2" data-postType="<?php echo get_the_id();?>" data-user="<?php echo $userId; ?>">Invia</button>
+                    <button type="button" class="btn-scopri btn-send-valutazione ps-3 pe-3 pt-2 pb-2" data-postType="<?php echo get_the_id(); ?>" data-user="<?php echo $userId; ?>">Invia</button>
                 </div>
              </div>
         </div>
     </div>
 </form>
-
-<?php get_footer();
+<?php } else {
+    $urlLogin = site_url('/login');
+    wp_safe_redirect($urlLogin);
+}
+get_footer();
