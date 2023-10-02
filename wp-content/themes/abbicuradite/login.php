9f6f808bc4bc8b6d56e1a2c7130fa3aae3f429bd
kella <kella@a-piu.it>
2023-10-02 12:17:29 +0200

    🐛 When login redirect area riservata

--- wp-content/themes/abbicuradite/login.php
+++ wp-content/themes/abbicuradite/login.php
@@ -1,28 +1,33 @@
 <?php /* Template Name: Login */
 
 get_header();
-?>
-<div class="main-content-login">
-    <div class="container d-flex align-items-center justify-content-center">
-        <div class="row my-5">
-            <div class="col">
-                <div class="content text-center">
-                    <h1 class="my-5"><?php the_title()?></h1>
-                    <div class="pageBody p-5 mb-5" >
-                        <div class="text-center mb-4">
-                            <img class="logo" src="<?php echo get_theme_file_uri('/images/logo.png'); ?>" alt="logo">
+$redirect_url = home_url('/area-test');
+if (is_user_logged_in()):
+    // Perform the redirect
+    header("Location: $redirect_url");
+    exit;
+else: ?>
+    <div class="main-content-login">
+        <div class="container d-flex align-items-center justify-content-center">
+            <div class="row my-5">
+                <div class="col">
+                    <div class="content text-center">
+                        <h1 class="my-5"><?php the_title()?></h1>
+                        <div class="pageBody p-5 mb-5" >
+                            <div class="text-center mb-4">
+                                <img class="logo" src="<?php echo get_theme_file_uri('/images/logo.png'); ?>" alt="logo">
+                            </div>
+
+                            <?php wp_login_form();?>
+                            <small>
+                                <?php wp_register('','',true);   ?>
+                            </small>
+
                         </div>
-
-                        <?php wp_login_form();?>
-                        <small>
-                            <?php wp_register('','',true);   ?>
-                        </small>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<?php
+<?php endif;
 get_footer();
